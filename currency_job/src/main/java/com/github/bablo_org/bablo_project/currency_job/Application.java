fa1f6cf47ea5a673cfe3e1b16fecd4d3e93b61ad
HappyCyborg <kornienko.pa@gmail.com>
2023-03-27 22:13:33 +0300

    feature/currency_job

---
+++ currency_job/src/main/java/com/github/bablo_org/bablo_project/currency_job/Application.java
@@ -0,0 +1,8 @@
+package com.github.bablo_org.bablo_project.currency_job;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
