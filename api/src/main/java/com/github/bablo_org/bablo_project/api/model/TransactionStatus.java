9b2af6bd1e520c6eb5080629e476100956e73ff4
avpetrykin <andrei.petrykin@billingplatform.com>
2023-04-01 20:07:23 +0700

    feature/squashes-generator

--- api/src/main/java/com/github/bablo_org/bablo_project/api/model/TransactionStatus.java
+++ api/src/main/java/com/github/bablo_org/bablo_project/api/model/TransactionStatus.java
@@ -1,24 +1,9 @@
 package com.github.bablo_org.bablo_project.api.model;
 
-import static java.util.stream.Collectors.toList;
-
-import java.util.List;
-
 public enum TransactionStatus {
     NEW,
     PENDING,
     APPROVED,
     DECLINED,
     COMPLETED;
-
-    public static List<TransactionStatus> ofList(List<String> names) {
-        if (names == null) {
-            return null;
-        }
-
-        return names
-                .stream()
-                .map(TransactionStatus::valueOf)
-                .collect(toList());
-    }
 }
