46bf98ed6b21580d46dc192dd0d4d1adf3d17556
HappyCyborg <kornienko.pa@gmail.com>
2023-04-05 21:27:17 +0300

    feature/currency_job

---
+++ api/src/main/java/com/github/bablo_org/bablo_project/api/job/CurrencyJob.java
@@ -0,0 +1,4 @@
+package com.github.bablo_org.bablo_project.api.job;
+
+public class CurrencyJob {
+}
--- api/src/main/java/com/github/bablo_org/bablo_project/api/model/Currency.java
+++ api/src/main/java/com/github/bablo_org/bablo_project/api/model/Currency.java
@@ -1,9 +1,13 @@
 package com.github.bablo_org.bablo_project.api.model;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.google.cloud.firestore.DocumentSnapshot;
 import lombok.Value;
+
+import static java.util.Optional.ofNullable;
 
 @Value
 public class Currency {
@@ -22,4 +26,17 @@ public class Currency {
                 doc.getDate("updated")
         );
     }
+
+    public Map<String, Object> toMap() {
+        Map<String, Object> map = new HashMap<>();
+
+        ofNullable(getId()).ifPresent(v -> map.put("id", v));
+        ofNullable(getName()).ifPresent(v -> map.put("name", v));
+        ofNullable(getSymbol()).ifPresent(v -> map.put("symbol", v));
+        ofNullable(getRate()).ifPresent(v -> map.put("rate", v));
+        ofNullable(getUpdated()).ifPresent(v -> map.put("updated", v));
+
+        return map;
+    }
+
 }
