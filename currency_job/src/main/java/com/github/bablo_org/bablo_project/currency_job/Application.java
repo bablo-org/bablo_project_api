0168716179eb7c38f83c9b5e135ecc0676e08a5d
HappyCyborg <kornienko.pa@gmail.com>
2023-04-05 20:11:42 +0300

    feature/currency_job

--- currency_job/src/main/java/com/github/bablo_org/bablo_project/currency_job/Application.java
+++ currency_job/src/main/java/com/github/bablo_org/bablo_project/currency_job/Application.java
@@ -13,22 +13,32 @@ import java.net.URL;
 public class Application {
 
     public static void main(String[] args) throws IOException {
+        if (1 == 2) {
 
 // Setting URL
-        String url_str = "https://v6.exchangerate-api.com/v6/ee38339f5db785019960d1dd/latest/USD";
+            String url_str = "https://v6.exchangerate-api.com/v6/ee38339f5db785019960d1dd/latest/USD";
 
 // Making Request
-        URL url = new URL(url_str);
-        HttpURLConnection request = (HttpURLConnection) url.openConnection();
-        request.connect();
+            URL url = new URL(url_str);
+            HttpURLConnection request = (HttpURLConnection) url.openConnection();
+            request.connect();
 
 // Convert to JSON
-        JsonParser jp = new JsonParser();
-        JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
-        JsonObject jsonobj = root.getAsJsonObject();
+            JsonParser jp = new JsonParser();
+            JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
+            JsonObject jsonobj = root.getAsJsonObject();
 
 // Accessing object
-        //String req_result = jsonobj.get("result").getAsString();
-        System.out.println(jsonobj);
+            //String req_result = jsonobj.get("result").getAsString();
+            System.out.println(jsonobj);
+        } else {
+
+            TestImportJson resource = new TestImportJson( );
+            String jsonString = resource.getResource("api response.json");
+
+            JsonObject jsonObject = JsonParser.parseString(jsonString).getAsJsonObject();
+            System.out.println(jsonObject);
+
+        }
     }
 }
