ed8463b81e0390fe8514366ff304f6666ebd7f4f
avpetrykin <andrei.petrykin@billingplatform.com>
2023-04-05 23:52:26 +0700

    feature/updateSettings method

--- api/src/main/java/com/github/bablo_org/bablo_project/api/service/CurrencyService.java
+++ api/src/main/java/com/github/bablo_org/bablo_project/api/service/CurrencyService.java
@@ -3,7 +3,6 @@ package com.github.bablo_org.bablo_project.api.service;
 import static com.google.cloud.firestore.FieldPath.documentId;
 import static com.google.cloud.firestore.Filter.equalTo;
 import static com.google.cloud.firestore.Filter.inArray;
-import static java.util.Optional.ofNullable;
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toMap;
 
@@ -13,7 +12,6 @@ import java.util.Map;
 import com.github.bablo_org.bablo_project.api.model.Currency;
 import com.github.bablo_org.bablo_project.api.model.User;
 import com.google.cloud.firestore.DocumentSnapshot;
-import com.google.cloud.firestore.FieldPath;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.WriteBatch;
 import lombok.RequiredArgsConstructor;
@@ -33,18 +31,6 @@ public class CurrencyService {
     @SneakyThrows
     public List<Currency> getAll() {
         return firestore.collection(COLLECTION_NAME)
-                .get()
-                .get()
-                .getDocuments()
-                .stream()
-                .map(Currency::ofDoc)
-                .collect(toList());
-    }
-
-    @SneakyThrows
-    public List<Currency> getById(List<String> ids) {
-        return firestore.collection(COLLECTION_NAME)
-                .where(inArray(FieldPath.documentId(), ids))
                 .get()
                 .get()
                 .getDocuments()
