1795286fc6cfc7031b3e690e6bfde5f15c92cb57
HappyCyborg <kornienko.pa@gmail.com>
2023-04-10 22:35:13 +0300

    feature/currency_job

--- api/src/main/java/com/github/bablo_org/bablo_project/api/model/CurrencyExternal.java
+++
@@ -1,8 +0,0 @@
-package com.github.bablo_org.bablo_project.api.model;
-
-import java.util.Date;
-
-public class CurrencyExternal {
-    String id;
-    Double rate;
-}
---
+++ api/src/main/java/com/github/bablo_org/bablo_project/api/model/currencyExternal/CurrencyExternal.java
@@ -0,0 +1,25 @@
+package com.github.bablo_org.bablo_project.api.model.currencyExternal;
+
+import lombok.Value;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.util.Optional.ofNullable;
+
+@Value
+public class CurrencyExternal {
+    String id;
+    Double rate;
+    Date updated;
+
+    public Map<String, Object> toMap() {
+        Map<String, Object> map = new HashMap<>();
+
+        ofNullable(getId()).ifPresent(v -> map.put("id", v));
+        ofNullable(getRate()).ifPresent(v -> map.put("rate", v));
+        ofNullable(getUpdated()).ifPresent(v -> map.put("updated", v));
+        return map;
+    }
+}
