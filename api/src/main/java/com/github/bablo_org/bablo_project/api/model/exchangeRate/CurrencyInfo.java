41fab10307561910dcc9382cd295b96a2c58763e
HappyCyborg <kornienko.pa@gmail.com>
2023-05-02 18:32:00 +0300

    feature/currency_job

--- api/src/main/java/com/github/bablo_org/bablo_project/api/model/exchangeRate/CurrencyInfo.java
+++ api/src/main/java/com/github/bablo_org/bablo_project/api/model/exchangeRate/CurrencyInfo.java
@@ -12,14 +12,12 @@ import static java.util.Optional.ofNullable;
 public class CurrencyInfo {
     String id;
     String name;
-    //String symbol;
 
     public Map<String, Object> toMap() {
         Map<String, Object> map = new HashMap<>();
 
         ofNullable(getId()).ifPresent(v -> map.put("id", v));
         ofNullable(getName()).ifPresent(v -> map.put("name", v));
-        //ofNullable(getSymbol()).ifPresent(v -> map.put("symbol", v));
         return map;
     }
 
