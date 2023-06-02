b5a2ce563207ac6855f94d2d36f51af570c07fd8
avpetrykin <andrei.petrykin@billingplatform.com>
2023-06-02 20:11:55 +0800

    feature/user-registration

--- api/src/main/java/com/github/bablo_org/bablo_project/api/controller/RegistrationController.java
+++ api/src/main/java/com/github/bablo_org/bablo_project/api/controller/RegistrationController.java
@@ -6,6 +6,7 @@ import com.github.bablo_org.bablo_project.api.service.RegistrationService;
 import com.google.firebase.auth.FirebaseToken;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,12 +20,18 @@ public class RegistrationController {
     private final RegistrationService service;
 
     @PostMapping("/register")
-    void register(@RequestAttribute(USER_TOKEN) FirebaseToken userToken) {
+    ResponseEntity<String> register(@RequestAttribute(USER_TOKEN) FirebaseToken userToken) {
         service.register(userToken.getUid(), userToken.getEmail());
+        return ResponseEntity
+                .ok()
+                .build();
     }
 
     @PostMapping("/unregister")
-    void unregister(@RequestAttribute(USER_TOKEN) FirebaseToken userToken) {
+    ResponseEntity<String> unregister(@RequestAttribute(USER_TOKEN) FirebaseToken userToken) {
         service.unregister(userToken.getUid());
+        return ResponseEntity
+                .ok()
+                .build();
     }
 }
